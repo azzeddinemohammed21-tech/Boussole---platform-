10c253877db078cd27be61a4c2e7d018d0f12043
azzeddinemohammed21-tech <azzeddinemohammed21@gmail.com>
2026-07-14 15:16:33 +0100

    Add robots function for metadata route

--- app/app/robots.ts
+++ app/app/robots.ts
@@ -1,1 +1,14 @@
 
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://boussole-platform.vercel.app/sitemap.xml",
+  };
+}
