3ba293543aae04e7e7486eb95b3f34b7b9d16aa9
azzeddinemohammed21-tech <azzeddinemohammed21@gmail.com>
2026-07-14 16:04:10 +0100

    Rename function from robots to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,10 @@
 import { MetadataRoute } from "next";
 
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://boussole-platform.vercel.app",
+      lastModified: new Date(),
     },
-    sitemap: "https://boussole-platform.vercel.app/sitemap.xml",
-  };
+  ];
 }
