3b05ee0bf239e73c86bad6b9358c6011ffcb848c
azzeddinemohammed21-tech <azzeddinemohammed21@gmail.com>
2026-07-22 23:43:20 +0100

    Add BCIDimension and BCILevel interfaces

--- types/bci.ts
+++ types/bci.ts
@@ -1,1 +1,13 @@
+export interface BCIDimension {
+  id: string;
+  name: string;
+  desc: string;
+  weight: number;
+}
 
+export interface BCILevel {
+  min: number;
+  max: number;
+  label: string;
+  desc: string;
+}
